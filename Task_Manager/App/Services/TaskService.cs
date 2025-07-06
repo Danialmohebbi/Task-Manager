52bfec2c58643f4d1ffe016a34265dfa86c04a82
DanialMohebbi <mohebbis@DanialMohebbis-MacBook-Pro.local>
2025-07-06 06:04:52 +0200

    Implement task services

--- Task_Manager/App/Services/TaskService.cs
+++ Task_Manager/App/Services/TaskService.cs
@@ -1,4 +1,6 @@
 using Task_Manager.App.Interfaces;
+using Task_Manager.Models;
+using Task = Task_Manager.Models.TaskItem;
 
 namespace Task_Manager.App.Services;
 
@@ -11,4 +13,61 @@ public class TaskService
         _repo = repo;
     }
 
+    
+    public IEnumerable<Models.TaskItem> ViewTasks(int student_id) => _repo.GetByStudentId(student_id);
+
+    public bool TaskExists(int task_id, int student_id)
+    {
+        return (from t in ViewTasks(student_id)
+                where t.Id == task_id
+                select t
+            ).Any();
+    }
+    public bool AddTask(Task task)
+    {
+        if (TaskExists(task.Id, task.StudentId))
+        {
+            return false;
+        }
+        _repo.Add(task);
+        return true;
+    }
+
+    public bool MarkTaskDone(int task_id)
+    {
+        var task = _repo.GetById(task_id);
+        if (task == null)
+            return false;
+
+        DateTime temp = DateTime.Now;
+        
+        _repo.Update(task with
+        {
+            Completed = true,
+            UpdatedAt = temp,
+            CompletedAt = temp
+        });
+        return true;
+    }
+    
+    public bool EditTask(Task updatedTask)
+    {
+        var existing = _repo.GetById(updatedTask.Id);
+        if (existing == null) return false;
+
+        _repo.Update(updatedTask);
+        return true;
+    }
+
+
+
+    public bool DeleteTask(int task_id, int student_id)
+    {
+        if (!TaskExists(task_id, student_id))
+        {
+            return false;
+        }
+        _repo.Delete(task_id);
+        return true;
+    }
 }
